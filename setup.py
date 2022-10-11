44667ef27b6a486cee3aaa5aea86c7e05913c061
Nicolás Carbonare <nicolas.carbonare@kit.edu>
2022-10-11 16:14:40 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(  
+    name='pyBuildSim', 
+    version='0.0.0',  
+    description='Python package for performing simplified building simulation',
+    url='https://github.com/nicocarbo/pyBuildSim',  
+    author='Nicolas Carbonare', 
+    author_email='nicocarbonare@gmail.com',  
+    packages=find_packages() 
+    )
