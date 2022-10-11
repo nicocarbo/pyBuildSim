b1e61ddea32db0c31eface017e13d1c6f436c6f4
Nicolás Carbonare <nicolas.carbonare@kit.edu>
2022-10-11 16:19:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
-from setuptools import setup, find_packages
+import os, setuptools
 from codecs import open
-from os import path
 
-setup(  
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, "requirements.txt")) as f:
+    required_packages = f.read().splitlines()
+    
+setuptools.setup(  
     name='pyBuildSim', 
     version='0.0.0',  
     description='Python package for performing simplified building simulation',
     url='https://github.com/nicocarbo/pyBuildSim',  
     author='Nicolas Carbonare', 
     author_email='nicocarbonare@gmail.com',  
-    packages=find_packages() 
+    packages=setuptools.find_packages(),
+    install_requires=required_packages,
+    include_package_data=True
     )
